515de6a5372dfb7d82dbfaf4013bd3fc03ac9839
Sergey Poterianski <spoterianski@gmail.com>
2022-10-28 15:35:17 +0300

    Reordered and added some files for build package and publication

---
+++ README.md
@@ -0,0 +1,39 @@
+# 1pass2pass
+
+Utility for transfer items from the 1password (*.1pif files) to the pass ([the standard unix password manager](https://www.passwordstore.org)).
+
+## Usage
+
+```python
+python 1pass2pass [-h] [-v] [-p] [-f] [-fl] <1pif filename> <folder>
+```
+
+Positional arguments:
+
+```console
+<1pif filename>     - path to *.1pif file for processing
+<folder>            - path to folder for store passwords
+```
+
+Optional arguments:
+
+```console
+-h,  --help         - show help message
+-v,  --verbose      - increase output verbosity
+-p,  --print-only   - print data into console, without saving into password store
+-f,  --force        - force overwrite existing passwords (default=False)
+-fl, --first-line   - Put password in first line (default=False)
+```
+
+## Example
+
+```console
+python 1pass2pass.py -v -f -fl ~/Downloads/1password.1pif /Personal
+```
+
+Where we import passwords from `~/Downloads/1password.1pif` to the folder `/Personal` with verbose output, force overwrite existing passwords and put password in first line.
+
+## Requirements
+
+- python 3.6+
+- loguru
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='1pass2pass',
+    version='0.0.1',
+    description='Utility for transfer items from the 1password (*.1pif files) to the pass',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Sergey Poterianski',
+    author_email='spoterianski@gmail.com',
+    license='MIT',
+    requires=['loguru'],
+    install_requires=['loguru'],
+    url='https://github.com/spoterianski/1pass2pass',
+    download_url='https://github.com/spoterianski/1pass2pass/tarball/v0.0.1',
+    scripts=['bin/1pass2pass'],
+    keywords= ("1pass2pass", "1password", "pass"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: The MIT License",
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        "Environment :: Console"
+    ],
+    python_requires='>=3.6'
+)
